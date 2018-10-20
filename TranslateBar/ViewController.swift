6228c8cf13c06036207f5dbce14cb71592aa94e7
Artem Bobrov <artembobrov3059@gmail.com>
2018-10-20 22:59:03 +0200

    Initial Commit

---
+++ TranslateBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TranslateBar
+//
+//  Created by Artem Bobrov on 20/10/2018.
+//  Copyright © 2018 Artem Bobrov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
---
+++ TranslateBarTests/TranslateBarTests.swift
@@ -0,0 +1,34 @@
+//
+//  TranslateBarTests.swift
+//  TranslateBarTests
+//
+//  Created by Artem Bobrov on 20/10/2018.
+//  Copyright © 2018 Artem Bobrov. All rights reserved.
+//
+
+import XCTest
+@testable import TranslateBar
+
+class TranslateBarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
